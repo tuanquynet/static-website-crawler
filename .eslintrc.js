3d4009e48757679655a192c851b398f549d620d7
Quy Tran <tuanquynet@gmail.com>
2019-05-26 22:26:27 +0700

    add eslint prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable quote-props,key-spacing */
+module.exports = {
+	'root': true,
+	'extends': ['airbnb-base'],
+	'rules': {
+		'no-console': 'off',
+	},
+	'globals': {
+	},
+	'env': {
+		'node': true,
+		'browser': true,
+		'es6': true,
+	},
+	'plugins': []
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: "es5",
+	tabWidth: 2,
+	useTabs: false,
+  semi: false,
+  singleQuote: true
+};
